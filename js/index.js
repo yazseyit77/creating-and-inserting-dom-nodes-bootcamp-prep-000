e7f5a8ade06baa8aa287a6c436fc678aaadfe4a1
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-07-26 04:43:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 var element = document.createElement('div');
+element.innerHTML = 'What\'s up world!';
+element.style.backgroundColor = '#green';
